6087270ef7b68e74fdc148f7f3099d8dc9dea09a
Alejo <alejoczombos@gmail.com>
2024-10-08 11:53:52 -0300

    Favicon y metadata

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,12 @@
 export function Footer() {
   return (
-    <footer className="bg-secondary text-secondary-foreground p-4 text-center w-full">
-      <p>Creado por: Tu Nombre</p>
+    <footer className="bg-secondary text-secondary-foreground p-4 text-center w-full text-sm">
+      <p>
+        Creado por:{" "}
+        <a className="text-blue" target="_blank" href="https://github.com/AlejoCzombos">
+          Alejo Czombos
+        </a>
+      </p>
     </footer>
   );
 }
